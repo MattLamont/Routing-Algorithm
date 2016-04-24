90dc10ab94b8c38c726046792b433bec00d68b29
MattLamont <matthewlamont454@gmail.com>
2016-04-23 23:32:29 -0600

    project setup

---
+++ build/CMakeFiles/routing_table.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/routing_table.dir/src/graph_example.cpp.o"
+  "routing_table.pdb"
+  "routing_table"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/routing_table.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
